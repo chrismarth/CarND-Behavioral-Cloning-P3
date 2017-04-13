c0f28bf267124e65119b5438825c959110950780
Christopher Marth <chrismarth@gmail.com>
2017-04-13 00:13:59 -0600

    Added left and right camera images and steer angles to data set. Added ability to combine multiple simulator collections to a single training set

--- model.py
+++ model.py
@@ -4,21 +4,20 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-
 import cv2
 import csv
 
-# Read each sample from the CSV data acquisition file
-data_prefix = 'data/baseline_2laps/'
-samples = []
-with open(data_prefix + 'driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-def generator(samples, batch_size=32):
+def generator(samples, steer_correction=0.2, batch_size=32):
+    """
+    Given a set of samples and a batch size, return an iterator that returns a 2-tuple of numpy arrays with the first
+    value in the tuple being the input image and the second value the label
+    
+    :param samples: The total number of test samples
+    :param steer_correction: 
+    :param batch_size: The size of the sample batch that the iterator will return
+    :return: An iterator that on iteration returns a 2-tuple of numpy arrays containing input data and label
+    """
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -27,36 +26,53 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = data_prefix + 'IMG/' + batch_sample[0].split('/')[-1]
-                center_image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
+                center_name = batch_sample[7] + 'IMG/' + batch_sample[0].split('/')[-1]
+                center_image = cv2.cvtColor(cv2.imread(center_name), cv2.COLOR_BGR2RGB)
+                left_image = cv2.cvtColor(cv2.imread(center_name), cv2.COLOR_BGR2RGB)
+                right_image = cv2.cvtColor(cv2.imread(center_name), cv2.COLOR_BGR2RGB)
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                left_angle = center_angle + steer_correction
+                right_angle = center_angle - steer_correction
+                images.extend(center_image, left_image, right_image)
+                angles.extend(center_angle, left_angle, right_angle)
 
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
+# Create the complete set of test samples from the set of CSV data acquisition files
+test_cases = ['data/baseline_2laps/']
+test_samples = []
+for test_case in test_cases:
+    with open(test_case + 'driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            line.append(test_case)
+            test_samples.append(line)
+
+# Create training and validation sets and create generators that iterates through each sets
+train_samples, validation_samples = train_test_split(test_samples, test_size=0.2)
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 # Setup model
 model = Sequential()
 
-# Pre-processing
-# 1. TODO: Trim top half of image
-# 2. Convert to grayscale
-# 3. Normalize and center between -1 and 1
-model.add(Lambda(lambda x: tf.image.rgb_to_grayscale(x), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(160,320,1)))
+# Pre-processing Layers
+# 1. TODO: Trim top half of image - eliminate the top 60 pixels since this is just background noise
+# 2. TODO: Possibly downsample to a smaller image size
+model.add(Lambda(lambda x: tf.image.rgb_to_grayscale(x), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(160, 320, 1)))
 
 # Network setup
-model.add(Flatten(input_shape=(160,320,1)))
+model.add(Flatten(input_shape=(160, 320, 1)))
 model.add(Dense(1))
 
 # Run backpropagation
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= len(train_samples),
                     validation_data=validation_generator, nb_val_samples=len(validation_samples),
-                    nb_epoch=3)
+                    nb_epoch=5)
+
+# Save the model so we can use it drive the vehicle
 model.save('model.h5')
